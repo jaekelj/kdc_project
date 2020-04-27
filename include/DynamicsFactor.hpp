b1f406f4811dfbec02659a017fbb95f294d38501
rogarg <rg1@andrew.cmu.edu>
2020-04-27 17:19:47 -0400

    added params to the preintegratedDynamicsMeas class

--- include/DynamicsFactor.hpp
+++ include/DynamicsFactor.hpp
@@ -4,7 +4,85 @@
 #include <gtsam/nonlinear/NonlinearFactor.h>
 #include <gtsam/base/Matrix.h>
 
+class PreintegratedCombDynamicsMeasurements : public PreintegrationType {
+
+  public:
+
+    struct DynamicsParams {
+      Eigen::Matrix<double, 3, 1> g_vec; // gravity vector
+      Eigen::Matrix<double, 3, 3> D; // drag matrix
+      Eigen::Matrix<double, 3, 3> dR; // rotation
+      
+      double dtij; // delta t from i to j
+      double dtij_1; // last dtij
+      double dt_1; // last dt
+      
+      Eigen::Matrix<double, 3, 1> dT_nav_1; // last dT_nav
+      Eigen::Matrix<double, 3, 3> dD_nav_1; // last dD_nav
+
+      Eigen::Matrix<double, 3, 1> Pi_g;
+      Eigen::Matrix<double, 3, 1> Pi_T;
+      Eigen::Matrix<double, 3, 3> Pi_D;
+      Eigen::Matrix<double, 3, 3> Pi_Dg;
+      Eigen::Matrix<double, 3, 1> Pi_DT;
+      Eigen::Matrix<double, 3, 3> Pi_DD;
+
+      Eigen::Matrix<double, 3, 1> SPi_g;
+      Eigen::Matrix<double, 3, 1> SPi_T;
+      Eigen::Matrix<double, 3, 3> SPi_D;
+      Eigen::Matrix<double, 3, 3> SPi_Dg;
+      Eigen::Matrix<double, 3, 1> SPi_DT;
+      Eigen::Matrix<double, 3, 3> SPi_DD;
+
+      DynamicsParams(Eigen::Matrix<double, 3, 3> drag_mat) : 
+                              g_vec(Vector3(0, 0, 9.81)),
+                              D(drag_mat),
+                              dR(I_3x3),
+                              dtij(0.0f),
+                              dtij_1(0.0f),
+                              dT_nav_1(Z_3x1),
+                              dD_nav_1(Z_3x3),
+                              Pi_g(Z_3x1),
+                              Pi_T(Z_3x1),
+                              Pi_D(Z_3x3),
+                              Pi_Dg(Z_3x3),
+                              Pi_DT(Z_3x1),
+                              Pi_DD(Z_3x3),
+                              SPi_g(Z_3x1),
+                              SPi_T(Z_3x1),
+                              SPi_D(Z_3x3),
+                              SPi_Dg(Z_3x3),
+                              SPi_DT(Z_3x1),
+                              SPi_DD(Z_3x3) {}
+    };
+
+  protected: 
+    /* Ideally there should be a covariance matrix of preintegrated measurements here
+     *
+     */
+
+    friend class DynamicsFactor;
+
+  public:
+    PreintegratedCombDynamicsMeasurements(Eigen::Matrix<double, 3, 3> drag_mat) {}
+    /**
+     * Add a single dynamics measurement to the preintegration
+     * 
+     */
+    void integrateMeasurement();
+    void resetParams();    
+};
+
 class DynamicsFactor : public NoiseModelFactor4<Pose3, Vector3, Pose3, Vector3>{
+    
+    private:
+    typedef DynamicsFactor This;
+    typedef NoiseModelFactor4<Pose3, Vector3, Pose3, Vector3> Base;
+
+    public: 
+    
+    
+    
     Vector evaluateError(const Pose3& pose_i, const Pose3& pose_j, const Vector3& vel_i, const Vector3& vec_i){
         // TODO: Implement this function
         Vector a;
