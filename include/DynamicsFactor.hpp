4daafd96310b9ddc5c7db7cdc24a10793cdc30af
rogarg <rg1@andrew.cmu.edu>
2020-04-28 17:57:20 -0400

    basic structure in place. methods need to be written out. builds.

--- include/DynamicsFactor.hpp
+++ include/DynamicsFactor.hpp
@@ -34,27 +34,28 @@ class PreintegratedCombDynamicsMeasurements : public PreintegrationType {
       Eigen::Matrix<double, 3, 1> SPi_DT;
       Eigen::Matrix<double, 3, 3> SPi_DD;
 
-      DynamicsParams(Eigen::Matrix<double, 3, 3> drag_mat) : 
-                              g_vec(Vector3(0, 0, 9.81)),
-                              D(drag_mat),
-                              dR(I_3x3),
-                              dtij(0.0f),
-                              dtij_1(0.0f),
-                              dT_nav_1(Z_3x1),
-                              dD_nav_1(Z_3x3),
-                              Pi_g(Z_3x1),
-                              Pi_T(Z_3x1),
-                              Pi_D(Z_3x3),
-                              Pi_Dg(Z_3x3),
-                              Pi_DT(Z_3x1),
-                              Pi_DD(Z_3x3),
-                              SPi_g(Z_3x1),
-                              SPi_T(Z_3x1),
-                              SPi_D(Z_3x3),
-                              SPi_Dg(Z_3x3),
-                              SPi_DT(Z_3x1),
-                              SPi_DD(Z_3x3) {}
+      DynamicsParams() : g_vec {Vector3{0, 0, 9.81}},
+                          D {Z_3x3},
+                          dR {I_3x3},
+                          dtij {0.0f},
+                          dtij_1 {0.0f},
+                          dT_nav_1 {Z_3x1},
+                          dD_nav_1 {Z_3x3},
+                          Pi_g {Z_3x1},
+                          Pi_T {Z_3x1},
+                          Pi_D {Z_3x3},
+                          Pi_Dg {Z_3x3},
+                          Pi_DT {Z_3x1},
+                          Pi_DD {Z_3x3},
+                          SPi_g {Z_3x1},
+                          SPi_T {Z_3x1},
+                          SPi_D {Z_3x3},
+                          SPi_Dg {Z_3x3},
+                          SPi_DT {Z_3x1},
+                          SPi_DD {Z_3x3} {}
     };
+
+    DynamicsParams dyn_params;
 
   protected: 
     /* Ideally there should be a covariance matrix of preintegrated measurements here
@@ -64,30 +65,35 @@ class PreintegratedCombDynamicsMeasurements : public PreintegrationType {
     friend class DynamicsFactor;
 
   public:
-    PreintegratedCombDynamicsMeasurements(Eigen::Matrix<double, 3, 3> drag_mat) {}
+    PreintegratedCombDynamicsMeasurements() : dyn_params() {}
     /**
      * Add a single dynamics measurement to the preintegration
      * 
      */
+    void setupDragMatrix(const Eigen::Matrix<double, 3, 3>& drag_mat);
     void integrateMeasurement();
     void resetParams();    
-};
+    void predictRotation();
+    void predictPosition();
+    void predictVelocity();
+
+}; // class PreintegratedCombDynamicsMeasurements
 
 class DynamicsFactor : public NoiseModelFactor4<Pose3, Vector3, Pose3, Vector3>{
     
-    private:
+  private:
     typedef DynamicsFactor This;
     typedef NoiseModelFactor4<Pose3, Vector3, Pose3, Vector3> Base;
 
-    public: 
-    
-    
-    
-    Vector evaluateError(const Pose3& pose_i, const Pose3& pose_j, const Vector3& vel_i, const Vector3& vec_i){
-        // TODO: Implement this function
-        Vector a;
-        return a;
-    }
-};
+    PreintegratedCombDynamicsMeasurements _PIDM_;
 
+  public: 
+    
+    void evaluateError(const Pose3& pose_i, const Pose3& pose_j, 
+                      const Vector3& vel_i, const Vector3& vec_i);
+
+    virtual ~DynamicsFactor() {}
+
+
+}; // class DynamicsFactor
 #endif
---
+++ src/DynamicsFactor.cpp
@@ -0,0 +1,37 @@
+#include <DynamicsFactor.hpp>
+
+//------------------------------------------------------------------------------
+// Inner class PreintegratedCombinedMeasurements methods
+//------------------------------------------------------------------------------
+void PreintegratedCombDynamicsMeasurements::setupDragMatrix(const Eigen::Matrix<double, 3, 3>& 
+                                                            drag_mat) {
+
+
+}
+
+void PreintegratedCombDynamicsMeasurements::predictRotation() {
+
+}
+
+void PreintegratedCombDynamicsMeasurements::predictVelocity() {
+
+}
+
+void PreintegratedCombDynamicsMeasurements::predictPosition() {
+
+}
+
+void PreintegratedCombDynamicsMeasurements::integrateMeasurement() {
+
+}
+
+
+
+//------------------------------------------------------------------------------
+// class DynamicsFactor methods
+//------------------------------------------------------------------------------
+void evaluateError(const Pose3& pose_i, const Pose3& pose_j, 
+                      const Vector3& vel_i, const Vector3& vec_i) {
+
+
+}
